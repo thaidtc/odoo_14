e2f5a2f83e1a50297f460fe57064ab99651db4c7
thaidtc <thaidtc@bap.jp>
2025-09-18 00:15:05 +0700

    [Hospital] Return Action and View From

--- custom/om_hospital/wizard/create_appointment.py
+++ custom/om_hospital/wizard/create_appointment.py
@@ -6,12 +6,36 @@ class CreateAppointmentWizard(models.TransientModel):
     _description = "Create Appointment Wizard"
 
     # Fields
-    date_appointment = fields.Date(string='Date', required=True)
+    date_appointment = fields.Date(string='Date')
     patient_id = fields.Many2one('hospital.patient', string='Patient', required=True)
 
     def create_appointment_action(self):
         values = {
             'patient_id': self.patient_id.id,
-            'date_appointment': self.date_appointment,
+            'date_appointment': self.date_appointment
         }
-        self.env['hospital.appointment'].create(values)
+        new_appointment = self.env['hospital.appointment'].create(values)
+        print('---> ', new_appointment)
+        return {
+            'name': _('Appointment'),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'hospital.appointment',
+            'res_id': new_appointment.id
+        }
+    
+    def view_appointment_action(self):
+        action = self.env.ref('om_hospital.appointment_action').read()[0]
+        action['domain'] = [('patient_id', '=', self.patient_id.id)]
+
+        # action = self.env['ir.actions.actions']._for_xml_id('om_hospital.appointment_action')
+        # action['domain'] = [('patient_id', '=', self.patient_id.id)]
+        return {
+            'name': 'Appointments',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'tree,form',
+            'res_model': 'hospital.appointment',
+            'domain': [('patient_id', '=', self.patient_id.id)],
+            'target': 'current'
+        }    
+        # return action
